df6b0ca0b58c99cd467d9b8b70c20a8f1b246a53
Zully56 <160067100+Zully56@users.noreply.github.com>
2024-03-11 16:48:27 -0300

    drop

---
+++ drop/productos.js
@@ -0,0 +1,8 @@
+class Promocoes{
+    constructor(promocoes,preço, des, marca){
+        this.promocoes = nome
+        this.preco = sobrenome 
+        this.des = idade 
+        this.marca = altura
+    }
+}
